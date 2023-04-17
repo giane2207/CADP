ef5a0e83c7563d1084c4725539ad58c4b42fe652
giane2207 <gianella2209@gmail.com>
2023-04-17 17:53:33 -0300

    ordenando

--- practica2/parte2/ejercicio10.pas
+++ practica2/parte2/ejercicio10.pas
@@ -1,2 +1,16 @@
-holiAAAAAAAAAA
+program ej10p2p2;
+
+function EsVocal (c: char): boolean;
+begin
+  EsVocal:= (c= 'a') or (c= 'e') or (c= 'i') or (c='o') or (c='u');
+  
+end;
+
+var
+ c: char;
+begin
+  readln (c);
+  if (EsVocal(c)) then 
+    writeln ('geniaaaaaaaaaaaaa');
+end.
 
