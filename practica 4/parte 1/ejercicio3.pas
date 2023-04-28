0ed7a46e6885b2e1316a4974f104910990dadfa9
giane2207 <gianella2209@gmail.com>
2023-04-27 22:31:19 -0300

    ordenando

--- practica 4/parte 1/ejercicio3.pas
+++ practica 4/parte 1/ejercicio3.pas
@@ -1,3 +1,14 @@
+{3. Se dispone de un vector con números enteros, de dimensión física dimF y dimensión lógica dimL.
+a) Realizar un módulo que imprima el vector desde la primera posición hasta la última.
+b) Realizar un módulo que imprima el vector desde la última posición hasta la primera.
+c) Realizar un módulo que imprima el vector desde la mitad (dimL DIV 2) hacia la primera posición, y
+desde la mitad más uno hacia la última posición.
+d) Realizar un módulo que reciba el vector, una posición X y otra posición Y, e imprima el vector desde la
+posición X hasta la Y. Asuma que tanto X como Y son menores o igual a la dimensión lógica. Y considere
+que, dependiendo de los valores de X e Y, podría ser necesario recorrer hacia adelante o hacia atrás.
+e) Utilizando el módulo implementado en el inciso anterior, vuelva a realizar los incisos a, b y c.}
+
+
 program ej3p3;
 
 const
@@ -17,7 +28,7 @@ begin
         dimL:= dimL + 1;
     end;
 end;
-
+{a) Realizar un módulo que imprima el vector desde la primera posición hasta la última.}
 procedure incisoA(v: vector; dimL: integer);
 var
     i: integer;
@@ -27,6 +38,7 @@ begin
     end;
 end;
 
+{b) Realizar un módulo que imprima el vector desde la última posición hasta la primera.}
 procedure incisoB (v: vector; dimL: integer);
 var 
      i: integer;
@@ -36,7 +48,8 @@ begin
      end;
 end;
 
-
+{c) Realizar un módulo que imprima el vector desde la mitad (dimL DIV 2) hacia la primera posición, y
+desde la mitad más uno hacia la última posición.}
 procedure incisoC (v: vector; dimL: integer);
 var 
      i: integer;
@@ -50,7 +63,9 @@ begin
      end; 
 end;        
         
-
+{d) Realizar un módulo que reciba el vector, una posición X y otra posición Y, e imprima el vector desde la
+posición X hasta la Y. Asuma que tanto X como Y son menores o igual a la dimensión lógica. Y considere
+que, dependiendo de los valores de X e Y, podría ser necesario recorrer hacia adelante o hacia atrás.}
 procedure xy(x,y: integer; v: vector);
 var i:integer;
 begin
